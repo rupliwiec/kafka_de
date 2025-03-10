fbf8cad3fe65440637ee54b3d67a8b78ef53efe6
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2025-03-10 14:36:54 +0100

    format data function result

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -18,6 +18,26 @@ def get_data():
      res = res['results'][0]
      return res
 
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
 def stream_data():
     import json
 
